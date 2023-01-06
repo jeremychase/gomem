6b8ff27cd58ad45e5c099081a1bf4ea7e19670eb
Jeremy Chase <jeremy.chase@gmail.com>
2023-01-06 11:02:36 -0500

    two mprofs

--- main.go
+++ main.go
@@ -3,42 +3,62 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"math/rand"
 	"os"
 	"runtime/pprof"
 )
 
-var memprofile = flag.String("memprofile", "", "write memory profile to this file")
 var iterations = flag.Uint("iterations", 1, "number of iterations to run")
+var mprofPrefix = flag.String("mprofPrefix", "output", "prefix for mprof files")
 
 var storage = map[int]int{}
 
-type data struct {
-	Key   int
-	Value int
-}
-
 func main() {
 	rand.Seed(42)
-	parseFlags()
+	mid, end := parseFlags()
+	defer mid.Close()
+	defer end.Close()
 
-	for *iterations > 0 {
-		*iterations--
+	i := uint(0)
+	for i < *iterations {
+		i++
 
 		err := leak()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if *iterations == 0 {
-			// Last run
+		// Write mid point mprof
+		if i*2 == *iterations {
+			fmt.Printf("writing %s at %d\n", mid.Name(), i)
+			err := pprof.WriteHeapProfile(mid)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		// Write final mprof
+		if i == *iterations {
+			fmt.Printf("writing %s at %d\n", end.Name(), i)
+			err := pprof.WriteHeapProfile(end)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 
 }
 
+// leak puts random values into a map while doing unnecessary work.
 func leak() error {
+	// data exists for marshalling purposes
+	type data struct {
+		Key   int
+		Value int
+	}
+
 	t := data{rand.Int(), rand.Int()}
 
 	// Marshal/Unmarshal into new value for spurious allocations
@@ -62,16 +82,22 @@ func leak() error {
 	return nil
 }
 
-func parseFlags() {
+func parseFlags() (*os.File, *os.File) {
 	flag.Parse()
 
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-
-			log.Fatal(err)
-		}
-		pprof.WriteHeapProfile(f)
-		defer f.Close()
+	if *iterations%2 != 0 {
+		log.Println("odd iteration count; adding one to allow mid point")
+		*iterations++
 	}
+
+	mid, err := os.Create(*mprofPrefix + "-mid.mprof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	end, err := os.Create(*mprofPrefix + "-end.mprof")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return mid, end
 }
